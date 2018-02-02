863865820a4551979a2d4ec018f47d70fcc1b32e
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-02 20:55:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,10 @@ function sayHiToGrandma(string){
   if (lowercase.toLowerCase() === lowercase){
     return "I can't hear you!"
     
-  }else if (uppercase.toUpperCase() === uppercase)
+    }else if (uppercase.toUpperCase() === uppercase)
   {
         return "YES INDEED"
+  }
+    
   }
 }
